c8db36d03c2f09f5c80bcc09e0f8824fd2797aad
Diegodsito <143525921+Diegodsito@users.noreply.github.com>
2024-04-25 22:11:36 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -118,7 +118,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 Pair * firstMap(HashMap * map) {
 
   if(map==NULL) return NULL;
-  long posicion = 0;
+  long posicion = map->current;
   Pair *current = map->buckets[posicion];
   
   while(current == NULL && posicion != map->size){
