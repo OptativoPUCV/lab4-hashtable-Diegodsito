d9712ad982f592a543e55cfb4f62cf1440c39b09
Diegodsito <143525921+Diegodsito@users.noreply.github.com>
2024-04-27 17:43:44 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -69,18 +69,16 @@ void enlarge(HashMap * map) {
 
   Pair** old_buckets = map->buckets;
   long old_capacity = map->capacity;
-  long nueva_capacidad = map->capacity * 2;
-
-  HashMap *nuevoMapa = createMap(nueva_capacidad);
+  map->capacity = map->capacity * 2;
+  map->buckets = (Pair**)malloc(sizeof(Pair*) * map->capacity);
+  map->size = 0;
   
-  for(long i = 0; i < old_capacity; i++){
-    if(old_buckets[i]->key != NULL){
-      insertMap(nuevoMapa, old_buckets[i]->key, old_buckets[i]->value);
+  for(long i = 0 ; i < old_capacity ; i++){
+    if(old_buckets[i] != NULL && old_buckets[i]->key != NULL){
+      insertMap(map, old_buckets[i]->key, old_buckets[i]->value);
+      map->size++;
     }
   }
-
-  free(map);
-}
 
 
 HashMap * createMap(long capacity) {
